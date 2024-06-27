ee5b51b680e9abc58fd90b833c07784cb3bfcf65
Chris <chrisbf11@gmail.com>
2024-06-27 02:28:35 -0600

    solicitudHorasExtras avance

---
+++ Vistas/solicitudHorasExtras.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlEmpresarial.Vistas
+{
+    public partial class solicitudHorasExtras : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                colaborador.Items.Add(new ListItem("Colaborador 1", "1"));
+                colaborador.Items.Add(new ListItem("Colaborador 2", "2"));
+            }
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
