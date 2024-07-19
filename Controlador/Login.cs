fb2da053e811a10d6017e24e41aa504d1a66fe0c
UserEMM <esteban.f.mata.mena@gmail.com>
2024-07-18 19:18:54 -0600

    nueva conexion a base de datos

--- Controlador/Login.cs
+++ Controlador/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,9 +17,31 @@ namespace ControlEmpresarial.Vistas
 
         protected void submit_Click(object sender, EventArgs e)
         {
-            string Pass = contrasena.Text;
-            System.Diagnostics.Debug.WriteLine($"Valor de Pass: {Pass}");
-            ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Login button clicked')", true);
+            string host = "138.59.135.33"; //IP
+            int port = 3306; 
+            string database = "tiusr38pl_gestion"; 
+            string username = "gestion"; 
+            string password = "Ihnu00&34"; 
+
+            // Cadena de conexión
+            string connectionString = $"Server={host};Port={port};Database={database};Uid={username};Pwd={password};";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexión establecida correctamente.");
+                string Pass = contrasena.Text;
+                System.Diagnostics.Debug.WriteLine($"Valor de Pass: {Pass}");
+                ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Login button clicked')", true);
+                System.Diagnostics.Debug.WriteLine($"conexion exitosa");
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "showalert", $"alert('Error: {ex.Message}');", true);
+            }
         }
     }
 }
