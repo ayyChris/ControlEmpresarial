1664dce3fda4caef1742b8fc13562236fcee5d87
jaim13 <jaimmartinez13@gmail.com>
2024-08-03 15:30:20 -0600

    Master page 3 con notificaciones y nombre

--- Vistas/Site3.Master.cs
+++ Vistas/Site3.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ControlEmpresarial.Controlador;
+using ControlEmpresarial.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,54 @@ namespace ControlEmpresarial.Vistas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarNombreUsuario();
+                CargarNotificaciones();
+            }
+        }
 
+        private void CargarNombreUsuario()
+        {
+            HttpCookie cookie = Request.Cookies["UserInfo"];
+            if (cookie != null)
+            {
+                string nombre = cookie["Nombre"];
+                string apellidos = cookie["Apellidos"];
+                lblNombre.Text = nombre + " " + apellidos;
+                lblNombre.Visible = true;
+            }
+            else
+            {
+                lblNombre.Text = "Error";
+                lblNombre.Visible = true;
+            }
+        }
+
+        private void CargarNotificaciones()
+        {
+            HttpCookie cookie = Request.Cookies["UserInfo"];
+            if (cookie != null)
+            {
+                if (int.TryParse(cookie["idEmpleado"], out int idEmpleado))
+                {
+                    NotificacionService service = new NotificacionService();
+                    List<Notificacion> notificaciones = service.ObtenerNotificaciones(idEmpleado);
+
+                    repeaterNotificaciones.DataSource = notificaciones;
+                    repeaterNotificaciones.DataBind();
+                }
+                else
+                {
+                    lblNombre.Text = "Error al extraer ID de empleado";
+                    lblNombre.Visible = true;
+                }
+            }
+            else
+            {
+                lblNombre.Text = "Cookie no encontrada";
+                lblNombre.Visible = true;
+            }
         }
     }
 }
