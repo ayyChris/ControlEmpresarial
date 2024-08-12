1bfe1c7cc4d4443f40a9854b3d3d5694c2c98d8f
jaim13 <jaimmartinez13@gmail.com>
2024-08-12 14:57:44 -0600

    busqueda si hay una llegada tardia

--- Vistas/PaginaPrincipal/MenuColaborador.aspx.cs
+++ Vistas/PaginaPrincipal/MenuColaborador.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ControlEmpresarial.Vistas.Pagina_Principal
 {
@@ -11,7 +9,69 @@ namespace ControlEmpresarial.Vistas.Pagina_Principal
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                // Obtener el ID del empleado de la cookie
+                string empleadoIdStr = Request.Cookies["idEmpleado"]?.Value;
 
+                if (int.TryParse(empleadoIdStr, out int empleadoId))
+                {
+                    DateTime fechaActual = DateTime.Today;
+                    VerificarInconsistencia(empleadoId, fechaActual);
+                }
+                else
+                {
+                    Response.Write("No se pudo obtener el ID del empleado de la cookie.");
+                }
+            }
+        }
+
+        private void VerificarInconsistencia(int empleadoId, DateTime fecha)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    SELECT COUNT(*) AS Inconsistencias
+                    FROM Entradas e
+                    JOIN Empleado emp ON e.idEmpleado = emp.idEmpleado
+                    JOIN horario h ON emp.idHorario = h.idHorario
+                    WHERE e.idEmpleado = @EmpleadoId
+                      AND e.DiaMarcado = @Fecha
+                      AND e.HoraEntrada > h.horaEntrada
+                      AND FIND_IN_SET(
+                          CASE 
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Monday' THEN 'Lunes'
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Tuesday' THEN 'Martes'
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Wednesday' THEN 'Miércoles'
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Thursday' THEN 'Jueves'
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Friday' THEN 'Viernes'
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Saturday' THEN 'Sábado'
+                              WHEN DATENAME(WEEKDAY, @Fecha) = 'Sunday' THEN 'Domingo'
+                          END, 
+                          h.diaSemana
+                      ) > 0";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EmpleadoId", empleadoId);
+                    command.Parameters.AddWithValue("@Fecha", fecha);
+
+                    connection.Open();
+                    int inconsistencias = (int)command.ExecuteScalar();
+                    connection.Close();
+
+                    if (inconsistencias > 0)
+                    {
+                        Response.Write($"Se encontraron inconsistencias para el empleado {empleadoId} en la fecha {fecha.ToShortDateString()}.");
+                    }
+                    else
+                    {
+                        Response.Write($"No se encontraron inconsistencias para el empleado {empleadoId} en la fecha {fecha.ToShortDateString()}.");
+                    }
+                }
+            }
         }
     }
 }
