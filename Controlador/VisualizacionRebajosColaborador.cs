98f3e0e33206384f4863ca4c7646f02e8c9cbe23
Chris <chrisbf11@gmail.com>
2024-08-13 06:33:05 -0600

    Rebajos frontend terminado

---
+++ Controlador/VisualizacionRebajosColaborador.cs
@@ -0,0 +1,30 @@
+﻿using ControlEmpresarial.Controlador;
+using ControlEmpresarial.Services;
+using ControlEmpresarial.Vistas.Pagina_Principal;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlEmpresarial.Vistas
+{
+    public partial class VisualizacionRebajosColaborador : Page
+    {
+        private string cadenaConexion = "Server=138.59.135.33;Port=3306;Database=tiusr38pl_gestion;Uid=gestion;Pwd=Ihnu00&34;";
+        private NotificacionService notificacionService = new NotificacionService();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void gvRebajos_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+           
+        }
+
+
+    }
+}
